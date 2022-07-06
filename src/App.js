925fdf7e2714df04cb7151d919a01af69e785b0a
damduoi <damduoi@gmail.com>
2022-07-06 17:40:32 +0700

    cấu hình css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
     return (
         <div className="App">
             <h1>Hello</h1>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
         </div>
     );
 }
