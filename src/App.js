f4b96f636b34ddf38a71b36334079409d68ab6ca
damduoi <damduoi@gmail.com>
2022-07-06 16:29:48 +0700

    Cấu hình path

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+      <h1>Hello</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
